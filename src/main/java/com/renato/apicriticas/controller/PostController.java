65a2052f95303bfde6623a13f55b8c8d3b2329bf
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-08-08 17:02:05 -0300

    pagaable

--- src/main/java/com/renato/apicriticas/controller/PostController.java
+++ src/main/java/com/renato/apicriticas/controller/PostController.java
@@ -1,9 +1,9 @@
 package com.renato.apicriticas.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +21,7 @@ public class PostController {
 	
 	@CrossOrigin
 	@GetMapping
-	public ResponseEntity<List<Post>> getFilmes(){
-		List<Post> posts = postRepository.findAll();
-		return ResponseEntity.ok(posts);
+	public Page<Post> getFilmes( @PageableDefault(size = 4) Pageable paginacao){
+		return postRepository.findAll(paginacao);
 	}
 }
--- src/main/java/com/renato/apicriticas/repository/PostRepository.java
+++ src/main/java/com/renato/apicriticas/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.renato.apicriticas.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.renato.apicriticas.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
 
+	Page<Post> findAll(Pageable page);
 }
